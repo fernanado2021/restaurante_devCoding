a43202630621267aa35f93ee80bf7605a57b2aee
AndyPT1907 <andypaultito02@gmail.com>
2024-06-25 10:04:07 -0500

    Base de menu

--- src/app/pages/ordenes/ordenes.component.ts
+++ src/app/pages/ordenes/ordenes.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-ordenes',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './ordenes.component.html',
   styleUrl: './ordenes.component.css'
 })
---
+++ src/app/service/orders.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrdersService {
+
+  constructor(private http: HttpClient) { }
+
+  private API_PERSONAL = "http://localhost:9090/orders";
+  //private API_PERSONAL = "http://localhost:3000/articulos";
+
+  getPersonal(): Observable <any>{
+    return this.http.get( this.API_PERSONAL)
+  }
+  getPersonalUnico(id: any):Observable<any>{
+    return this.http.get( `${this.API_PERSONAL}/${id}` )
+  }
+  ////////////////GUARDAR- POST//////////////
+  postPersonal(persona:JSON): Observable<any>{
+    return this.http.post(this.API_PERSONAL, persona)
+  }
+  ///////////////Eliminar////////////////////////////////
+
+
+deletePersonalID(id:any): Observable<any>{
+  return this.http.delete(`${this.API_PERSONAL}/${id}`)
+}
+///////////////////EDITAR//////////////////////
+putNosotros(persona:any):Observable <any>{
+  return this.http.put(`${this.API_PERSONAL}/${persona.id}`,persona)
+}
+}
